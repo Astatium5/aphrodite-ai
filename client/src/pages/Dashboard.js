bfbf68e88bdc4eaa5ed3120a502edfcaf86efbc3
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-04-17 13:43:26 -0400

    Add table to dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,137 @@
+import Navbar from "../components/Navbar";
+import { PlusIcon, ChevronDownIcon } from "@heroicons/react/outline";
+
 const Dashboard = (props) => {
-  console.log(props);
+  const people = [
+    {
+      name: "Abraham Hernandez",
+      email: "h.abraham@outlook.com",
+      title: "Software Engineer",
+      role: "Frontend Lead",
+    },
+    {
+      name: "Abraham Hernandez",
+      email: "h.abraham@outlook2.com",
+      title: "Software Engineer",
+      role: "Frontend Lead",
+    },
+    {
+      name: "Abraham Hernandez",
+      email: "h.abraham@outlook3.com",
+      title: "Software Engineer",
+      role: "Frontend Lead",
+    },
+  ];
+  // console.log(props.location.state);
   return (
-    <div>
-      <p>Test</p>
+    <div className="flex flex-col items-center w-screen h-screen">
+      <Navbar />
+      <div className="my-20 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="flex flex-row w-full px-4 mb-1">
+            <span className="flex flex-1 flex-row justify-start text-gray-700">
+              <div className="inline-flex flex-row hover:text-gray-400 cursor-pointer">
+                <p>Add New</p>
+                <PlusIcon className="w-4" />
+              </div>
+            </span>
+            <span className="flex flex-1 flex-row justify-end text-gray-700">
+              <div className="inline-flex flex-row hover:text-gray-400 cursor-pointer">
+                <p>Result</p>
+                <ChevronDownIcon className="w-4" />
+              </div>
+            </span>
+          </div>
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Title
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Role
+                  </th>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">View</span>
+                  </th>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">Delete</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {people.map((person) => (
+                  <tr key={person.email}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="ml-4">
+                          <div className="text-sm font-medium text-gray-900">
+                            {person.name}
+                          </div>
+                          <div className="text-sm text-gray-500">
+                            {person.email}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">
+                        {person.title}
+                      </div>
+                      <div className="text-sm text-gray-500">
+                        {person.department}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                        Active
+                      </span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {person.role}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <button
+                        href="#"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        View
+                      </button>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <button
+                        href="#"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
