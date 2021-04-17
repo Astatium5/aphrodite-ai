436974737abc99c9d630f64d836dafe0068a22f7
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-04-16 21:26:13 -0400

    Adds navbar navigation

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,23 @@
 import logo from "../assets/navbar_logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="w-full h-16 bg-rose flex items-center shadow-lg">
-      <img src={logo} alt="Aphrodite.ai" className="ml-4 w-16" />
+      <Link to="/">
+        <img src={logo} alt="Aphrodite.ai" className="ml-4 w-16" />
+      </Link>
 
       <div className="flex-1 flex justify-end space-x-8 mr-8">
-        <button className=" text-gray-700">Sign in</button>
-        <button className="bg-button px-4 py-1 rounded-xl text-gray-100">
+        <Link className="text-gray-700" to="/signin">
+          Sign in
+        </Link>
+        <Link
+          className="bg-button px-4 py-1 rounded-xl text-gray-100"
+          to="/signup"
+        >
           Sign up
-        </button>
+        </Link>
       </div>
     </div>
   );
