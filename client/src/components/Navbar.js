589729ebc9bc061cacdac82d77b45930947b0c61
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-04-16 21:30:00 -0400

    Hotfix: navbar styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,11 +5,11 @@ const Navbar = () => {
   return (
     <div className="w-full h-16 bg-rose flex items-center shadow-lg">
       <Link to="/">
-        <img src={logo} alt="Aphrodite.ai" className="ml-4 w-16" />
+        <img src={logo} alt="Aphrodite.ai" className="ml-4 w-16 pb-2" />
       </Link>
 
       <div className="flex-1 flex justify-end space-x-8 mr-8">
-        <Link className="text-gray-700" to="/signin">
+        <Link className="text-gray-700 px-4 py-1" to="/signin">
           Sign in
         </Link>
         <Link
